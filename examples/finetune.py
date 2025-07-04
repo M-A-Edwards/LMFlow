fb3d70c210ca94981686a6cb38f4a200521f515f
Mark <93010414+M-A-Edwards@users.noreply.github.com>
2025-07-04 23:31:53 +0530

    Update finetune.py

--- examples/finetune.py
+++ examples/finetune.py
@@ -18,7 +18,8 @@ import sys
 import os
 script_dir = os.path.abspath(os.path.dirname(sys.argv[0])) 
 if script_dir in sys.path:
-    sys.path.remove(script_dir)from transformers import HfArgumentParser 
+    sys.path.remove(script_dir)
+from transformers import HfArgumentParser 
 
 from lmflow.args import (
     ModelArguments,

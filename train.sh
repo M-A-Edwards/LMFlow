9ac140b8aacaa4118bbc0e41105486803e49c2ee
Mark <93010414+M-A-Edwards@users.noreply.github.com>
2025-09-06 04:00:09 +0530

    Update train.sh

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@
 
 # Parses arguments
 model_name_or_path=data4elm/Llama-400M-12L
-dataset_path=./data/wikitext-2-raw-v1
+dataset_path=/kaggle/input/try1-heurstic/filtered_climblab
 # conversation_template=llama2
 output_dir=output_models/finetune
 deepspeed_args="--master_port=11000"
